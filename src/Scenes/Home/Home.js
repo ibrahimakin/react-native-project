46fe67c8b3497ff00ab9bb0d9b301d9dbf65f44a
ibrahim AKIN <ibrahim-akin@msn.com>
2020-08-21 23:19:20 +0300

    add api & instagram icons

--- src/Scenes/Home/Home.js
+++ src/Scenes/Home/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
-import { Image, Text, View, FlatList, ActivityIndicator, TouchableOpacity, Alert } from 'react-native';
+import { Image, Text, View, FlatList, ActivityIndicator, Dimensions, TouchableOpacity, Alert } from 'react-native';
 import { getList, removeData } from "../../Actions"
 import { connect } from 'react-redux';
+const { width, height } = Dimensions.get('window')
+
 
 const Home = (props) => {
 
@@ -26,16 +28,26 @@ const Home = (props) => {
     }
 
     const renderItem = ({ item }) => (
-        <View style={{ margin: 10, justifyContent: 'space-between', flexDirection: 'row' }}>
+        <View style={{ margin: 10, alignItems: 'center', justifyContent: 'space-between' }}>
             {item.image ?
-                <Image source={{ uri: item.image }} resizeMode='contain' style={{ height: 200, width: 200, }} />
+                <Image source={{ uri: item.image }} resizeMode='contain' style={{ height: width, width: '100%', }} />
                 : /* defaultSource={require('../../Image/head.png')} */
-                <Image source={require('../../Image/head.png')} resizeMode='contain' style={{ height: 200, width: 200, }} />
+                <Image source={require('../../Image/head.png')} resizeMode='contain' style={{ height: width, width: '100%', }} />
             }
-            <View style={{ justifyContent: 'space-evenly', alignItems: 'flex-end' }}>
+            <View style={{ justifyContent: 'space-between', width: '100%', flexDirection: 'row' }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Image source={require('../../Image/instagram_like.png')} style={{ height: 25, width: 25, marginRight: 10 }} />
+                    <Image source={require('../../Image/instagram_comment.png')} style={{ height: 25, width: 25, marginRight: 10 }} />
+                    <Image source={require('../../Image/forward_outline.png')} style={{ height: 25, width: 25, marginRight: 10 }} />
+                </View>
+
+
                 <TouchableOpacity onPress={() => { deleteClick(item) }}>
-                    <Image style={{ alignItems: 'right' }} source={require('../../Image/delete.png')} style={{ height: 20, width: 20 }} />
+                    <Image source={require('../../Image/delete.png')} style={{ height: 25, width: 25 }} />
                 </TouchableOpacity>
+
+            </View>
+            <View style={{ justifyContent: 'space-between', width: '100%', flexDirection: 'row' }}>
                 <Text style={{ margin: 10 }}>{item.name}</Text>
                 <Text style={{ margin: 10 }}>{item.status}</Text>
                 <Text style={{ margin: 10 }}>{item.species}</Text>
